acab863a47043898d05fa9d7742173c91ba4ff79
mehfooz-analyst5 <analyst5@ugspk.com>
2024-03-15 09:47:48 +0500

    integrated chart js

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -12,3 +13,11 @@ class Food(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Consume(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    food_consumed = models.ForeignKey(Food, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.food_consumed.name} - {self.user.username}"
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from . models import Food
+from . models import Food, Consume
 
 
 
@@ -8,5 +8,13 @@ from . models import Food
 
 def home(request):
     foods = Food.objects.all()
+    consumed_food = Consume.objects.filter(user=request.user)
 
-    return render(request, 'app/index.html', {'foods': foods})
+    if request.method == 'POST':
+        food_consumed = request.POST['food_consumed']
+        food = Food.objects.get(name=food_consumed)
+        user = request.user
+        consume = Consume(user=user, food_consumed=food)
+        consume.save()
+
+    return render(request, 'app/index.html', {'foods': foods, 'consumed_food': consumed_food})
